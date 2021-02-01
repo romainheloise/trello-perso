56da1a28a4b01bfec41f25a725d5767aff55f610
romheloise <romheloise@gmail.com>
2021-02-01 17:05:29 +0100

    start drag and drop

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { addItem, displayMenu } from "../slice/counterSlice";
 import Item from "./Item";
 import ColumnMenu from "./ColumnMenu";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Column = ({ name, list, dispatch, index, column }) => {
   const [input, setInput] = useState("");
@@ -22,28 +23,41 @@ const Column = ({ name, list, dispatch, index, column }) => {
         {column.dash[index].menuDisplay && <ColumnMenu index={index} />}
       </div>
       <div className="column-items-container">
-        {column.dash[index].addItemDisplay && (
-          <div className="column-additems">
-            <input
-              type="text"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="item title"
-            />
-            <button
-              id={index}
-              onClick={(e) => {
-                dispatch(addItem({ id: parseInt(e.target.id), title: input }));
-                setInput("");
-              }}
-            >
-              add
-            </button>
-          </div>
-        )}
-        {list.map((item, index) => {
-          return <Item {...item} key={index} />;
-        })}
+        <DragDropContext>
+          <Droppable droppableId={index}>
+            {(provided) =>
+              column.dash[index].addItemDisplay && (
+                <div
+                  className="column-additems"
+                  {...provided.droppableProps}
+                  innerRef={provided.innerRef}
+                >
+                  <input
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="item title"
+                  />
+                  <button
+                    id={index}
+                    onClick={(e) => {
+                      dispatch(
+                        addItem({ id: parseInt(e.target.id), title: input })
+                      );
+                      setInput("");
+                    }}
+                  >
+                    add
+                  </button>
+                  {provided.placeholder}
+                </div>
+              )
+            }
+          </Droppable>
+          {list.map((item, index) => {
+            return <Item {...item} key={index} />;
+          })}
+        </DragDropContext>
       </div>
     </div>
   );
